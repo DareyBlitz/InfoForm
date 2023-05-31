96950c8045a16790662bd88110af0d1174affcda
Darey Blitz <dareyblitz@vk.com>
2023-05-31 19:45:08 +0300

    Initial commit

---
+++ src/main/java/org/example/Post.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Post {
+    public String surname;
+    public String name;
+    public String patronymic;
+    public String passport;
+    public String phoneNumber;
+    public boolean subscription;
+    public FormDate birthday;
+
+
+
+}
